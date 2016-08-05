471902a3f94a2a09396c8eedafa31922a89ee4df
htan712 <hock.tan712@gmail.com>
2016-08-05 09:11:44 -0700

    figuring out firebasearray

--- dist/scripts/controllers/TodoCtrl.js
+++ dist/scripts/controllers/TodoCtrl.js
@@ -1,13 +1,14 @@
 (function() {
     function TodoCtrl($scope, $firebaseArray) {
-        var task = new Firebase('https://markoff-9194b.firebaseio.com/');
-        
-        $scope.todos = $firebaseArray(task);
-        $scope.addTodo = function(){
+                
+        /** I'm not exactly sure how to use firebasearray correctly here */
+        $scope.todos = $firebaseArray(firebase.database().ref().child('/todo'));
+        $scope.addTodos = function(){
+            $scope.todos.
             console.log("Add toto");
         };
         
-    };
+    }
         angular
             .module('markOff')
             .controller('TodoCtrl', ['$scope', '$firebaseArray', TodoCtrl]);
